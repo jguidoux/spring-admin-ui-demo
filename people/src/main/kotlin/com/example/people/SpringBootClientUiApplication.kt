20f90fb79eab5ba4ae0e4c32f32abc8a38b74bbe
jguidux <jeremie.guidoux@slickteam.fr>
2022-03-13 20:44:26 +0100

    add repo

--- people/src/main/kotlin/com/example/people/SpringBootClientUiApplication.kt
+++ people/src/main/kotlin/com/example/people/SpringBootClientUiApplication.kt
@@ -1,23 +1,36 @@
 package com.example.people
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
+import kotlin.collections.HashMap
 
 @SpringBootApplication
-open class SpringBootClientUiApplication
+class SpringBootClientUiApplication {
+	@Bean
+	fun init(repository: PeopleRepository) = CommandLineRunner {
+		listOf("jerem", "karin", "Batman" )
+			.map { People(name = it) }
+			.forEach{ repository.save(it) }
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SpringBootClientUiApplication>(*args)
-}
+	}
+
 
 @Configuration
-open class SecurityPermitAllConfig : WebSecurityConfigurerAdapter() {
+class SecurityPermitAllConfig : WebSecurityConfigurerAdapter() {
 	@Throws(Exception::class)
 	override fun configure(http: HttpSecurity) {
 		http.authorizeRequests().anyRequest().permitAll()
@@ -25,16 +38,19 @@ open class SecurityPermitAllConfig : WebSecurityConfigurerAdapter() {
 	}
 }
 
-data class People(val id: String?, val name: String)
+data class People(val id: String = UUID.randomUUID().toString(), val name: String)
 
 
 @RestController
 @RequestMapping("/people")
-class MessageResource {
+class MessageResource(private val repository: PeopleRepository) {
 	@GetMapping
-	fun index(): List<People> = listOf(
-		People("1", "jeremie!"),
-		People("2", "karine!"),
-		People("3", "Batman!"),
-	)
+	fun allPeople() =  repository.findAll()
+}
+
+@Component
+class PeopleRepository(val peoples: HashMap<String, People> = hashMapOf()) {
+	fun findAll() = peoples.values
+	fun save(newGuy: People) = peoples.put(newGuy.id, newGuy)
+
 }
